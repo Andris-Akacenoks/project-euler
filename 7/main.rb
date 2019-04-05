a7e6cd8312a6e25ae00ab071cf9237cabcfa1a82
andris.akacenoks <andris.akacenoks@testdevlab.com>
2019-04-05 21:14:23 +0300

    Task 7 done

---
+++ 7/main.rb
@@ -0,0 +1,25 @@
+# Problem 7: 10001st prime
+# Answer: 104743
+
+def is_prime?(number)
+  for i in 2..number-1
+    return false if number % i == 0
+  end
+  true
+end
+
+def get_prime_by_order(target_position)
+  order = 0
+  i = 2
+  while order < target_position
+    if is_prime?(i)
+      order += 1 
+      if order == target_position
+        return i
+      end
+    end
+    i += 1
+  end
+end 
+
+puts get_prime_by_order(10001)
